e58a0005d0144a6100016cf446e67192ac870bff
tjkenmate <tjkenmate@gmail.com>
2016-04-11 15:39:02 -0400

    Update Map.java with Gabe's code

--- Map.java
+++ Map.java
@@ -8,15 +8,15 @@ package com.senerade.game;
  */
 public class Map {
 	private String mapName = "";
-	private int[][] map = null;
+	private byte[][] map = null;
 	
 	//Constructor
 	public Map(int width, int height, String mapName){
-		map = new int [width][height];
-		for (int i = 0; i < width; i++)
-			for (int j = 0; j < height; j++)
+		map = new byte[width][height];
+		for (int i = 0; i < width; ++i)
+			for (int j = 0; j < height; ++j)
 				map[i][j] = 0;
-		this.setMapName(mapName);
+		this.mapName=mapName;
 	}
 	
 	public String getMapName() {
@@ -28,15 +28,30 @@ public class Map {
 	}
 	
 	//Returns the memory position of the array
-	public int[][] getMap(){
+	public byte[][] getMap(){
 		return map;
 	}
 	
-	public void setMapSpace(int x, int y, int value){
+	/**
+	* @author Gabriel Tofvesson
+	* 
+	* For debugging purposes.
+	*/
+	public String getMapAsText(){
+		String s = "";
+		for(int[] i : map){
+			for(int j : i)
+				s+=j+" ";
+			s+="\r\n"; //Proper linefeed
+		}
+		return s;
+	}
+	
+	public void setMapSpace(int x, int y, byte value){
 		map[x][y] = value;
 	}
 	
-	public int getMapSpace(int x, int y){
+	public byte getMapSpace(int x, int y){
 		int value = map[x][y];
 		return value;
 	}
@@ -71,8 +86,8 @@ public class Map {
 	
 	public static void main(String[] args){
 		Map a = new Map(10, 20, "phi");
-		for (int i = 0; i < 10; i++){
-			for (int j = 0; j < 20; j++){
+		for (int i = 0; i < 10; ++i){
+			for (int j = 0; j < 20; ++j){
 				a.setMapSpace(i, j, (i+j)%6);
 				System.out.print(a.getMapSpace(i, j) + " ");
 			}
@@ -80,12 +95,13 @@ public class Map {
 		}
 		System.out.println(a.getMap());
 		System.out.println(a.getMapName());
+		System.out.println(a.getMapAsText());
 		BattleActor d = new BattleActor("Mother", "Hi7");
 		System.out.println(d.getX() + " " + d.getId() + " " + d.getAtk());
 		BattleActor e = new BattleActor("x", "Hi7");
 		BattleMove f = new BattleMove(35);
-		Move.move(e, a);
-		//while(BattleActions.hit(d, e, f))
+		while(BattleActions.hit(d, e, f))
 			System.out.println("truex");
 	}
 }
+
